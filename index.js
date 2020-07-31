2065fde94bcff28192187aca934ee9d9d5ed85d7
SKCC <SKCC@SKCC18D00089.SKCC.NET>
2020-07-31 14:32:02 +0900

    eee

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('HEHEHEHEHEHHE!');
+    res.send('HEHEHEHEHEHHE gfgfgfgf!');
 });
 
 app.get('/start', function(req, res){
