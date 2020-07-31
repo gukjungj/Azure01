cbeb51cbcc17083a07ac91a5ef858ffe203f037f
gukjungj <54527107+gukjungj@users.noreply.github.com>
2020-07-31 14:52:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('Test02 !');
+    res.send('Test03 !');
 });
 
 app.get('/start', function(req, res){
